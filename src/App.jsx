d3cca79f5e362336621970b41ed973eb101af757
Xpanetel <ibrohim@MAC.local>
2024-03-18 16:10:36 +0500

    home

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "../../scss/Header.scss";
+import navi from "../assets/nav-i1.svg";
+import navIcon from "../assets/navIcon.svg";
+import navIcon2 from "../assets/navIcon2.svg";
+import navIcon3 from "../assets/navIcon3.svg";
+import navIcon4 from "../assets/navIcon4.svg";
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+          <Link to="/">
+            <Navbar.Brand as="span">
+              <img src={navi} alt="LOGO" />
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="">
+              <div className="nav-p">
+                <Link to="/">
+                  <Nav.Link as="span">
+                    <p>Home</p>
+                  </Nav.Link>
+                </Link>
+                <Link to="/products">
+                  <Nav.Link as="span">
+                    <p>Shop</p>
+                  </Nav.Link>
+                </Link>
+                <Link to="/about">
+                  <Nav.Link as="span">
+                    <p>About</p>
+                  </Nav.Link>
+                </Link>
+                <Link to="/contact">
+                  <Nav.Link as="span">
+                    <p>Contact</p>
+                  </Nav.Link>
+                </Link>
+              </div>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="navIcon">
+          <img src={navIcon} alt="" />
+          <img src={navIcon2} alt="" />
+          <img src={navIcon3} alt="" />
+          <img src={navIcon4} alt="" />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
